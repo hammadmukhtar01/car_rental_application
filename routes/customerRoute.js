5830a1d7500e02e1e396ab598507273d1dd5124f
hammad8634 <hammadmukhtar1122@gmail.com>
2024-07-25 12:14:30 +0400

    Add profile photo field in customer API

--- routes/customerRoute.js
+++ routes/customerRoute.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const customerController = require('../controllers/customer/customerController');
 const customerauthController = require('../controllers/customer/customerauthController');
-
+const multer = require('multer');
+const path = require('path');
 const router = express.Router();
+
+// Multer Config 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); 
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+    const extension = path.extname(file.originalname);
+    const filename = `${uniqueSuffix}.${extension}`;
+    cb(null, filename); 
+  },
+});
+
+const upload = multer({ storage });
 
 router.route('/all').get(customerController.getAllUsers);
 
 // router.route('/create').post(customerauthController.signup, customerauthController.sendEmailConfirm);
-router.route('/create').post(customerauthController.signup);
+router.route('/create').post(upload.single('customerProfileImg'), customerauthController.signup);
 
 router.route('/login').post(customerauthController.login);
 router
