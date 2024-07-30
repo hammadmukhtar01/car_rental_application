7bc67ca59a330e3c298526262e970edbe256ba96
hammad8634 <hammadmukhtar1122@gmail.com>
2024-07-30 12:02:34 +0400

    Removed unused customer routes

--- routes/customerRoute.js
+++ routes/customerRoute.js
@@ -3,13 +3,15 @@ const customerController = require('../controllers/customer/customerController')
 const customerauthController = require('../controllers/customer/customerauthController');
 const multer = require('multer');
 const router = express.Router();
-const storage = multer.memoryStorage(); 
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 router.route('/all').get(customerController.getAllUsers);
 
 // router.route('/create').post(customerauthController.signup, customerauthController.sendEmailConfirm);
-router.route('/create').post(upload.single('customerProfileImg'), customerauthController.signup);
+router
+  .route('/create')
+  .post(upload.single('customerProfileImg'), customerauthController.signup);
 
 router.route('/login').post(customerauthController.login);
 router
@@ -23,20 +25,16 @@ router.patch(
   customerauthController.updateCustomerProfile
 );
 
-// router.patch('/emailconfirm/:token', customerauthController.emailConfirm);
-
 router.patch(
   '/updatepassword',
   customerauthController.protect,
   customerauthController.updatePass
 );
 
-router
-  .route('/profile/:id')
-  .get(
-    customerauthController.protect,
-    // customerauthController.verifyProfileOwnership,
-    customerController.getProfile
-  );
+router.route('/profile/:id').get(
+  customerauthController.protect,
+  // customerauthController.verifyProfileOwnership,
+  customerController.getProfile
+);
 
 module.exports = router;
