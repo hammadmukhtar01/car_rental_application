f17b676fffd11e695cb27f2afc26a2fd8a6afc45
hammad8634 <hammadmukhtar1122@gmail.com>
2024-08-08 18:51:15 +0400

    Add email template for signup

--- utils/email.js
+++ utils/email.js
@@ -30,6 +30,34 @@ const sendMail = async (options) => {
         htmlContent = htmlContent.replace('{{fname}}', options.fname);
         htmlContent = htmlContent.replace('{{lname}}', options.lname);
 
+        let dynamicContent = '';
+
+        if (options.type === 'signup') {
+          dynamicContent = `
+             <div class="sign-up-email">
+                <p style="font-size: 16px">
+                  Login password is: <b>${options.password}</b>
+                </p>
+                <p style="text-align: left">
+                  <b>Note:</b> Password is the combination of your First Name
+                  and last 4 digits of your Phone Number. You can change your
+                  password anytime after log In at <b><a href="https://www.milelecarrental.com">Milele Car Rental</a></b>.
+                </p>
+              </div>
+          `;
+        } else if (options.type === 'contactus') {
+          dynamicContent = `
+            <div class="contact-us-form-email">
+                <h3 style="font-size: 18px">
+                  We have received your inquiry. You will be contacted by our
+                  experts soon!
+                </h3>
+              </div> 
+          `;
+        }
+
+        htmlContent = htmlContent.replace('{{dynamicContent}}', dynamicContent);
+
         mailOptions = {
           from: `Milele Car Rental <${process.env.EMAIL_USERNAME}>`,
           to: options.email,
